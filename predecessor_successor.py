86875c21aab7a13873a39da847a75963d3c7fa4a
SamylleRose <samylle1brito@gmail.com>
2023-08-30 17:30:44 -0300

    first commit

---
+++ predecessor_successor.py
@@ -0,0 +1,4 @@
+number = int(input(' Digite um número: '))
+predecessor = number - 1
+successor = number + 1
+print (' O antecessor de {} é {} e o sucessor é {}.'. format(number, predecessor, successor))
