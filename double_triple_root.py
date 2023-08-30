6b7cf2bbf4ee52ff4ccbdc1b47544ca995ddb419
SamylleRose <samylle1brito@gmail.com>
2023-08-30 17:41:47 -0300

    up

---
+++ double_triple_root.py
@@ -0,0 +1,5 @@
+number = int(input('Digite um número: '))
+double = number * 2
+triple = number *3
+root = number ** (1/2)
+print('Dobro: {} \nTriplo: {} \nRaiz quadrada: {}'. format(double, triple, root))
