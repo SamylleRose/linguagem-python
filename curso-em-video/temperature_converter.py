ead7f5fe859285c12741aaf35bc9b1468db17783
SamylleRose <samylle1brito@gmail.com>
2023-10-02 12:44:57 -0300

    up

---
+++ curso-em-video/temperature_converter.py
@@ -0,0 +1,3 @@
+temperature = float(input('Informe a temperatura em °C: '))
+converter_fahrenheit = ((9 * temperature) / 5) + 32
+print(f'A temperatura de {temperature}°C corresponde a {converter_fahrenheit}°F!')
