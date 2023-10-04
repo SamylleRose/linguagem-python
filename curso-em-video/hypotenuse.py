c105bf4f7ae1ae5ec4ffe0b79132db010b199b1a
SamylleRose <samylle1brito@gmail.com>
2023-10-04 19:54:40 -0300

    up

--- curso-em-video/hypotenuse.py
+++ curso-em-video/hypotenuse.py
@@ -1,9 +1,9 @@
-import math
+from math import hypot
 
 cateto_opposite = float(input('Digite o comprimento do cateto oposto: '))
 cateto_adjacent= float(input('Digite comprimento do cateto adjacente: '))
 # hypotenuse = (cateto_opposite ** 2 + cateto_adjacent ** 2) ** (1/2) 
-hypotenuse = math.hypot(cateto_opposite, cateto_adjacent)
+hypotenuse = hypot(cateto_opposite, cateto_adjacent)
 print(f'A hipotenusa é {hypotenuse:.2f}')
 
 
