08835e80ad6eef15bd8efa9a81eb5b4ba736ced0
SamylleRose <samylle1brito@gmail.com>
2023-11-08 17:01:02 -0300

    up

--- poo-I/praticas-na-aula/lista_compreensivas.py
+++ poo-I/praticas-na-aula/lista_compreensivas.py
@@ -3,6 +3,6 @@ n = 13
 divisores = [x for x in range(1, n + 1) if n % x == 0]
 
 if len(divisores) <= 2:
-    print("Eh primo")
+    print("E primo")
 else:
     print("ne primo n")
--- poo-I/praticas-na-aula/reduce.py
+++ poo-I/praticas-na-aula/reduce.py
@@ -1,10 +1,11 @@
 import functools as ft
 
+
 def soma(x, y):
-    return x + y
+    return x - y
+
 
 lista = [1, 2, 3, 4, 5]
 resultado = ft.reduce(soma, lista)
 
 print(resultado)
-
